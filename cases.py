51347f703240cdb6be9dd780094520277213e8d8
Dhiraj Tadikamalla <dtadikam@umich.edu>
2020-12-03 23:15:12 -0500

    Starting with Databases

--- cases.py
+++ cases.py
@@ -8,15 +8,30 @@ import os
 1. pull cases from each day for 100 days (USA)
 2. Total Cases until now (100 countries) vs. GDP
 '''
-def setUpDatabase(db_name):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-    sql = '''CREATE TABLE IF NOT EXISTS CASES (
-        DATE CHAR(10),
-        CASES INTEGER)'''
-    cur.execute(sql)
-    return cur, conn
+class dataBase:
+
+    def readDataFromFile(self, filename):
+        full_path = os.path.join(os.path.dirname(__file__), filename)
+        f = open(full_path, encoding='utf-8')
+        file_data = f.read()
+        f.close()  
+        json_data = json.loads(file_data)
+        return json_data
+
+    def setUpDatabase(self, db_name):
+        path = os.path.dirname(os.path.abspath(__file__))
+        conn = sqlite3.connect(path+'/'+db_name)
+        cur = conn.cursor()
+        return cur, conn
+
+    def setUpCasesTable(self, cur, conn):
+        functionData = dayCase.get_data(self)
+        cur.execute('CREATE TABLE Cases (Date CHAR(10), Cases INTEGER)')
+        for date, cases in functionData:
+            cur.execute('INSERT INTO Cases (Date, Cases) VALUES (?, ?)', (date, cases))
+            
+
+        return cur, conn
 
 
 class dayCase:
@@ -32,16 +47,15 @@ class dayCase:
         request = requests.get(request_url)
         jsons = json.loads(request.text)
         data = jsons.get('data')
+        dateCases = []
         for date in data:
-            dates = date
             USA = data[date].get('USA')
             if USA is None:
                 continue
             else:
                 cases = USA.get('confirmed')
-        INSERT INTO CASES (Date, Cases)
-            
-        return jsons
+                dateCases.append((date, cases))
+        return dateCases
 
 
 
@@ -49,7 +63,12 @@ def main():
     USA_cases = dayCase('2020-08-01', '2020-12-01')
     USA_cases.create_request_url()
     USA_cases.get_data()
-    cur, conn = setUpDatabase('Covid_Cases_USA.db')``
+    cur, conn = dataBase.setUpDatabase('Covid_Cases_USA.db')
+    dataBase.setUpCasesTable(cur, conn)
+
+    # json_data = readDataFromFile('yelp_data.txt')
+    # setUpCategoriesTable(json_data, cur, conn)
+    
 
 if __name__ == "__main__":
 	main()
