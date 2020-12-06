6c1bbc7c35118b150626b1e5f4c64e1a80479071
Saya <swtakai@umich.edu>
2020-12-05 20:51:33 -0500

    add 25 data points at a time

--- gdp.py
+++ gdp.py
@@ -25,7 +25,7 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def setUpGDPTable(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS GDP (country TEXT, current INTEGER, previous INTEGER, date CHAR(6))')
+    cur.execute('CREATE TABLE IF NOT EXISTS GDP (id INTEGER, country TEXT, current INTEGER, previous INTEGER, date CHAR(6))')
     all_data = get_data()
     country_list = []
     GDP_list = []
@@ -33,20 +33,27 @@ def setUpGDPTable(cur, conn):
     date_list = []
     n = 0
     x = 25
+    cur.execute('SELECT id FROM GDP ORDER BY id DESC LIMIT 1')
+    try:
+        last_entry = cur.fetchone()[0]
+        for a in all_data:
+            if a[0] == last_entry:
+                n = last_entry + 1
+                x = n + 25
+    except:
+        last_entry = 0
     for i in range(len(all_data[n:x])):
-        data = all_data[i]
-        country = data[0]
+        data = all_data[n + i]
+        country_id = data[0]
+        country = data[1]
         country_list.append(country)
-        GDP = data[1]
+        GDP = data[2]
         GDP_list.append(GDP)
-        previous_GDP = data[2]
+        previous_GDP = data[3]
         previous_GDP_list.append(previous_GDP)
-        date = data[3]
+        date = data[4]
         date_list.append(date)
-        cur.execute('INSERT INTO GDP (country, current, previous, date) VALUES (?, ?, ?, ?)', (country, GDP, previous_GDP, date,))
-        n += 25
-        x += 25
-
+        cur.execute('INSERT INTO GDP (id, country, current, previous, date) VALUES (?,?, ?, ?, ?)', (country_id, country, GDP, previous_GDP, date,))
     conn.commit()
 
 # 
@@ -88,6 +95,7 @@ def get_data():
     soup = BeautifulSoup(request.text, 'html.parser') 
     table = soup.find('div', class_='table-responsive')
     rows = table.find_all('tr', class_='datatable-row')
+    number = 0
     for row in rows:
         datas = row.find_all('td')
         country_data = []
@@ -95,11 +103,13 @@ def get_data():
             num = data.text
             stripped = num.strip()
             country_data.append(stripped)
+        country_id = number
         country = country_data[0]
         current = country_data[1]
         previous = country_data[2]
         update_month = country_data[3]
-        all_data.append((country, current, previous, update_month))
+        all_data.append((country_id, country, current, previous, update_month))
+        number = number + 1
     rows = table.find_all('tr', class_='datatable-row-alternating') 
     for row in rows:
         datas = row.find_all('td')
@@ -108,11 +118,13 @@ def get_data():
             num = data.text
             stripped = num.strip()
             country_data.append(stripped)
+        country_id = number
         country = country_data[0]
         current = country_data[1]
         previous = country_data[2]
         update_month = country_data[3]
-        all_data.append((country, current, previous, update_month))
+        all_data.append((country_id, country, current, previous, update_month))
+        number = number + 1
     return all_data
 
 
