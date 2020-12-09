90d2bb879ac924f51a25e12c8d8b91d4c27abba0
swtakai <47376024+swtakai@users.noreply.github.com>
2020-12-09 05:41:48 -0500

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -26,56 +26,51 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def setUpCasesTable(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS Stringencies (Date CHAR(10), Stringencies INTEGER)')
-    all_cases = get_data('2020-08-01', '2020-12-01')
-    n = 0
-    x = 25
-    cur.execute('SELECT Date FROM Stringencies ORDER BY Date DESC LIMIT 1')
-    try:
-        last_entry = cur.fetchone()[0]
-        for index in range(len(all_cases)):
-            if all_cases[index][0] == last_entry:
-                n = index + 1
-                x = n + 25
-    except:
-        last_entry = 0
+# def setUpCasesTable(cur, conn):
+#     cur.execute('CREATE TABLE IF NOT EXISTS Stringencies (Date CHAR(10), Stringencies INTEGER)')
+#     all_cases = get_data('2020-08-01', '2020-12-01')
+#     n = 0
+#     x = 25
+#     cur.execute('SELECT Date FROM Stringencies ORDER BY Date DESC LIMIT 1')
+#     try:
+#         last_entry = cur.fetchone()[0]
+#         for index in range(len(all_cases)):
+#             if all_cases[index][0] == last_entry:
+#                 n = index + 1
+#                 x = n + 25
+#     except:
+#         last_entry = 0
 
-    for i in range(len(all_cases[n:x])):
-        data = all_cases[n + i]
-        date = data[0]
-        cases = data[1]
-        cur.execute('INSERT INTO Stringencies (Date, Stringencies) VALUES (?,?)', (date, cases))
-    conn.commit()
-
-    # for i in range(5):
-    #     specific_date = datetime(2020, 8, 1)
-    #     start_date1 = specific_date + timedelta(x)
-    #     startpoint = (str((start_date1)).split()[0])
-    #     new_date = specific_date + timedelta(n)
-    #     endpoint = (str((new_date)).split()[0])
-    #     functionData = get_data(startpoint, endpoint)
-    #     for i in functionData:
-    #         cur.execute('INSERT INTO Stringencies (Date, Stringencies) VALUES (?, ?)', (i[0], i[1]))
-    #     n += 25
-    #     x += 25
-
-    # functionData1 = get_data('2020-08-26', '2020-09-20')
-    # for i in functionData1:
-    #     cur.execute('INSERT INTO Cases (Date, Cases) VALUES (?, ?)', (i[0], i[1]))
-    # functionData2 = get_data('2020-09-21', '2020-10-16')
-    # for i in functionData2:
-    #     cur.execute('INSERT INTO Cases (Date, Cases) VALUES (?, ?)', (i[0], i[1]))
-
-#class dayCase:
-
-    #def __init__(self, start_date, end_date):
-    #    self.start_date = start_date
-    #    self.end_date = end_date
+#     for i in range(len(all_cases[n:x])):
+#         data = all_cases[n + i]
+#         date = data[0]
+#         cases = data[1]
+#         cur.execute('INSERT INTO Stringencies (Date, Stringencies) VALUES (?,?)', (date, cases))
+#     conn.commit()
 
 def cases_per_day(cur, conn):
-    cur.execute('SELECT Cases FROM Stringencies ORDER BY Date DESC LIMIT 1')
-    return dateStringencies
+    cur.execute('SELECT Cases FROM Cases ORDER BY Date ASC LIMIT 1')
+    first_cases = cur.fetchone()[0]
+    cur.execute('SELECT Cases FROM Cases ORDER BY Date DESC LIMIT 1')
+    last_cases = cur.fetchone()[0]
+    cur.execute('SELECT Cases FROM Cases')
+    num_days = cur.fetchall()
+    avg_cases = (last_cases - first_cases) / len(num_days)
+    return avg_cases
+
+def percentage_recovered(cur, conn):
+    cur.execute('SELECT Country_ID, Cases, Recovered FROM CountryData')
+    percentage_list = []
+    all_data = cur.fetchall()
+    for data in all_data:
+        country_id = data[0]
+        cases = data[1]
+        recovered = data[2]
+        percent = (recovered / cases) * 100
+        percentage_list.append((country_id, percent))
+    return percentage_list
+
+
 
 
 
@@ -84,9 +79,14 @@ def main():
     # USA_cases.create_request_url()
     # USA_cases.get_data()
     # create_request_url('2020-08-01', '2020-12-01')
-    get_data('2020-08-01', '2020-12-01')
+
+
+
+
     cur, conn = setUpDatabase('Covid_Cases_USA.db')
-    setUpCasesTable(cur, conn)
+    cases_per_day(cur, conn)
+    percentage_recovered(cur, conn)
+    # setUpCasesTable(cur, conn)
 
     conn.close()
     # json_data = readDataFromFile('yelp_data.txt')
